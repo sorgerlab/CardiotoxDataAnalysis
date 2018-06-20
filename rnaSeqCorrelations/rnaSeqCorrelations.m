5abb378ed2f503d8ff5ac1bc64925b98e76c7024
Robert Sheehan <sheehan.robertp@gmail.com>
2018-06-20 12:18:54 -0400

    Comment and funtionalize final code

--- rnaSeqCorrelations/rnaSeqCorrelations.m
+++ rnaSeqCorrelations/rnaSeqCorrelations.m
@@ -1,3 +1,5 @@
+function [r,pvalue] = rnaSeqCorrelations()
+
 load('dmsoRNA.mat')
 load('erlotinibRNA.mat')
 load('lapatinibRNA.mat')
@@ -7,100 +9,101 @@ load('sunitinibRNA.mat')
 
 replicates = {'Rep 1', 'Rep 2', 'Rep 3'};
 
-% Pearson correlation of TMM-normalized counts data between replicates of DMSO treatment at 24 hour and Sorafenib treatment at 10µM and 24 hours were plotted. 
+%Plot earson correlation of TMM-normalized counts data between replicates 
+%Using 10µM and 24 hours treatment conditions
+%Corresponding Data in columns 4:6 in DMSO data, 10:12 in TKI Data
 
-%%%%Correlation%%%%%
-%Want 10 µM and 24 hours, so high dose, mid time
-%So So 2 - D replicates - 'S2D1','S2D2','S2D3'
-% dmso 4:6
-%Columns 10, 11, 12 for tkis.
+%%%%% DMSO %%%%%
 
-
+%Extract and log transform counts data
 dmso_10u_24hr = dmsoRNA(:,4:6);
 logdmso = log10(dmso_10u_24hr);
 logdmso(logdmso == inf) = NaN;
 logdmso(logdmso == -inf) = NaN;
 
+%Plot correlations between replicates
 figure;
-[r2,pvalue] = corrplot(logdmso,'varNames',namesToUse);
+[r,pvalue] = corrplot(logdmso,'varNames',replicates);
 
+%Set axis label options
 hfig = gcf;
 haxes = findobj(hfig, 'Type', 'Axes');
 set(haxes,'Fontname','Arial','FontWeight','Bold','Fontsize',12)
 set(0,'defaultfigurecolor',[1 1 1])
 
-fn1 = 'dmso.svg';
-print(fn1, '-Painters', '-dsvg','-r600')
+%%%%% Erlotinib %%%%%
 
-
-
-
+%Extract and log transform counts data
 erl_10u_24hr = erlotinibRNA(:,10:12);
 logerl = log10(erl_10u_24hr);
 logerl(logerl == inf) = NaN;
 logerl(logerl == -inf) = NaN;
 
-[r2,pvalue] = corrplot(logerl,'varNames',namesToUse);
+%Plot correlations between replicates
+figure;
+[r,pvalue] = corrplot(logerl,'varNames',replicates);
 
+%Set axis label options
 hfig = gcf;
 haxes = findobj(hfig, 'Type', 'Axes');
 set(haxes,'Fontname','Arial','FontWeight','Bold','Fontsize',12)
 set(0,'defaultfigurecolor',[1 1 1])
 
-fn1 = 'erl.svg';
-print(fn1, '-Painters', '-dsvg','-r600')
+%%%%% Lapatinib %%%%%
 
-
-
+%Extract and log transform counts data
 lapat_10u_24hr = lapatinibRNA(:,10:12);
 loglapat = log10(lapat_10u_24hr);
 loglapat(loglapat == inf) = NaN;
 loglapat(loglapat == -inf) = NaN;
 
-[r2,pvalue] = corrplot(loglapat,'varNames',namesToUse);
+%Plot correlations between replicates
+figure;
+[r,pvalue] = corrplot(loglapat,'varNames',replicates);
 
-
+%Set axis label options
 hfig = gcf;
 haxes = findobj(hfig, 'Type', 'Axes');
 set(haxes,'Fontname','Arial','FontWeight','Bold','Fontsize',12)
 set(0,'defaultfigurecolor',[1 1 1])
 
-fn1 = 'lap.svg';
-print(fn1, '-Painters', '-dsvg','-r600')
+%%%%% Sorafenib %%%%%
 
-
-% figure;
+%Extract and log transform counts data
 soraf_10u_24hre = sorafenibRNA(:,10:12);
 logSoraf = log10(soraf_10u_24hre);
 logSoraf(logSoraf == inf) = NaN;
 logSoraf(logSoraf == -inf) = NaN;
 
- [r2,pvalue] = corrplot(logSoraf,'varNames',namesToUse);
+%Plot correlations between replicates
+figure;
+[r,pvalue] = corrplot(logSoraf,'varNames',replicates);
 
- hfig = gcf;
-haxes = findobj(hfig, 'Type', 'Axes');
-set(haxes,'Fontname','Arial','FontWeight','Bold','Fontsize',12)
-set(0,'defaultfigurecolor',[1 1 1])
-
-fn1 = 'sor.svg';
-print(fn1, '-Painters', '-dsvg','-r600')
-
-
-
-sunit_10u_24hr = sunitinibRNA(:,10:11);
-logsunit = log10(sunit_10u_24hr);
-logsunit(logsunit == inf) = NaN;
-logsunit(logsunit == -inf) = NaN;
-
-[r2,pvalue] = corrplot(logsunit,'varNames',namesToUse([1 3]));
-
+%Set axis label options
 hfig = gcf;
 haxes = findobj(hfig, 'Type', 'Axes');
 set(haxes,'Fontname','Arial','FontWeight','Bold','Fontsize',12)
 set(0,'defaultfigurecolor',[1 1 1])
 
-fn1 = 'sun.svg';
-print(fn1, '-Painters', '-dsvg','-r600')
+%%%%% Sunitinib %%%%%
+
+%Extract and log transform counts data
+sunit_10u_24hr = sunitinibRNA(:,10:11);
+logsunit = log10(sunit_10u_24hr);
+logsunit(logsunit == inf) = NaN;
+logsunit(logsunit == -inf) = NaN;
+
+%Plot correlations between replicates
+figure;
+[r,pvalue] = corrplot(logsunit,'varNames',replicates([1 3]));
+
+%Set axis label options
+hfig = gcf;
+haxes = findobj(hfig, 'Type', 'Axes');
+set(haxes,'Fontname','Arial','FontWeight','Bold','Fontsize',12)
+set(0,'defaultfigurecolor',[1 1 1])
+
+
 
 
 
